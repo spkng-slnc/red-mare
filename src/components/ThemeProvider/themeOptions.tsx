f2449f2033c6beaa5ea2118785f69e0e7805fe66
laura <laura.bhayani@gmail.com>
2023-04-03 12:00:26 -0400

    add variants to theme

--- src/components/ThemeProvider/themeOptions.tsx
+++ src/components/ThemeProvider/themeOptions.tsx
@@ -4,21 +4,39 @@ export const themeOptions: ThemeOptions = {
   palette: {
     mode: "light",
     primary: {
+      // charcoal
+      dark: "#2E3B44",
+      light: "#4C616F",
       main: "#273E47",
     },
     secondary: {
+      // rust
+      dark: "#9F380C",
+      light: "#E25011",
       main: "#B1492F",
     },
     error: {
+      // burgundy
+      dark: "#6D001B",
+      light: "#B6002E",
       main: "#721121",
     },
     warning: {
+      // pumpkin
+      dark: "#F06000",
+      light: "#FF893A",
       main: "#F0803C",
     },
     success: {
+      // dark slate gray
+      dark: "#294444",
+      light: "#447272",
       main: "#384D48",
     },
     info: {
+      // vanilla
+      dark: "#ECD477",
+      light: "#F5E8B6",
       main: "#E1D89F",
     },
   },
