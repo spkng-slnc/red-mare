d832abcd1a032e4e5a204fe27206ad07b4f6b4c8
Laura Bhayani <laura.bhayani@gmail.com>
2023-08-09 18:08:36 -0400

    add feature: biography. remove @apollo/client

---
+++ src/features/Biography.tsx
@@ -0,0 +1,7 @@
+import { FeatureTemplate } from "../components/Templates/FeatureTemplate";
+
+const BIO = "Insert copy";
+
+export const Biography = () => {
+  return <FeatureTemplate copy={[BIO]} title={"Sarah Bhayani"} />;
+};
