660a9f9a21d969e39b93e91221ea42214e228bf0
laura <laura.bhayani@gmail.com>
2024-04-04 12:54:23 -0400

    add vitest, testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { defineConfig, UserConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr()] as UserConfig["plugins"],
   resolve: {
     alias: {
       src: `${path.resolve(__dirname, "./src")}`,
     },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
   },
 });
